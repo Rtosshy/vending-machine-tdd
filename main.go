a89f09a857fd559761b35e32ba25c3b273be6584
yingtian <haidouyingtian@gmail.com>
2026-06-22 21:59:27 +0900

    turn to Tosshy

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
 func main() {
+}
 
+type VendingMachine struct {
+	button  Button
+	balance int
+}
+
+func (v *VendingMachine) InsertCoin(coin int) {
+	v.balance += coin
+}
+
+func (v *VendingMachine) GetBalance() int {
+	return v.balance
+}
+
+func NewVendingMachine(button Button, balance int) *VendingMachine {
+	return &VendingMachine{button: button, balance: balance}
 }
 
 type Button struct {
--- main_test.go
+++ main_test.go
@@ -14,3 +14,19 @@ func TestPushButton(t *testing.T) {
 		t.Errorf("got: %v, expected: %v", got, expected)
 	}
 }
+
+func TestInsertCoin(t *testing.T) {
+	// coin := 100
+}
+
+func TestGetbalance(t *testing.T) {
+	name := "test"
+	mockButton := NewButton(name)
+	balance := 100
+	v := NewVendingMachine(*mockButton, balance)
+	got := v.GetBalance()
+	expected := 100
+	if got != expected {
+		t.Errorf("got: %v, expected: %v", got, expected)
+	}
+}
