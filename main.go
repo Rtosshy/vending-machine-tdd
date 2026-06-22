93df7fff579dcdd4e7ea7600f258cd830d47b084
Rtosshy <tosshy820@gmail.com>
2026-06-22 21:24:55 +0900

    chore: update and change turn

--- main.go
+++ main.go
@@ -3,3 +3,15 @@ package main
 func main() {
 
 }
+
+type Button struct {
+	name string
+}
+
+func (b *Button) Push() string {
+	return b.name
+}
+
+func NewButton(name string) *Button {
+	return &Button{name: name}
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestPushButton(t *testing.T) {
+	name := "cola"
+	b := NewButton(name)
+	got := b.Push()
+	expected := "cola"
+
+	if got != expected {
+		t.Errorf("got: %v, expected: %v", got, expected)
+	}
+}
